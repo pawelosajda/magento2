831d1f345ed1e1c5b057c5741b41b9457710d5bb
Paweł Osajda <p.osajda@advox.pl>
2017-05-01 16:12:21 +0200

    blog

--- app/code/Osajda/Blog/Model/ResourceModel/Post.php
+++ app/code/Osajda/Blog/Model/ResourceModel/Post.php
@@ -11,6 +11,6 @@ class Post extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
 {
     protected function _construct()
     {
-        $this->_init('blog_post', \Osajda\Blog\Api\Data\PostInterface::POST_ID);
+        $this->_init('osajda_blog_post', \Osajda\Blog\Api\Data\PostInterface::POST_ID);
     }
 }
